d6e0947fea3606ac1578985297e710724df41b0c
Victor Landa <victorlanda.dev@gmail.com>
2024-05-25 22:29:45 -0600

    base
    init shadcn/ui
    overwrite tailwind.config.ts
    format some files

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,16 @@
-"use client"
+'use client'
 
-import { usePathname } from "next/navigation"
+import { usePathname } from 'next/navigation'
 
 export default function Template({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname()
+  const pathname = usePathname()
 
-    return (
-        <div className="animate-appear p-4">
-            <pre className="inline-block bg-muted px-2 py-1 rounded-lg">{pathname}</pre>
-            {children}
-        </div>
-    )
+  return (
+    <div className='animate-appear p-4'>
+      <pre className='inline-block bg-muted px-2 py-1 rounded-lg'>
+        {pathname}
+      </pre>
+      {children}
+    </div>
+  )
 }
